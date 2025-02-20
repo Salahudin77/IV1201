d3199564e43ed3bda84d83b60656497da843074b
Salahudin77 <sasalah@kth.se>
2025-02-20 13:30:23 +0100

    Minor Changes

--- iv1201Group12/src/main/java/kth/iv1201/Group12/application/AvailabilityService.java
+++ iv1201Group12/src/main/java/kth/iv1201/Group12/application/AvailabilityService.java
@@ -2,15 +2,18 @@ package kth.iv1201.Group12.application;
 
 
 import kth.iv1201.Group12.entity.Availability;
+import kth.iv1201.Group12.entity.Person;
 import kth.iv1201.Group12.repository.AvailabilityRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class AvailabilityService {
 
     private AvailabilityRepository availabilityService;
+    private PersonService personService;
 
     public AvailabilityService(AvailabilityRepository availabilityService) {
         this.availabilityService = availabilityService;
@@ -18,5 +21,12 @@ public class AvailabilityService {
     public List<Availability>getAllavailabilities(){
         return availabilityService.findAll();
     }
-}
+
+    /*public String availablePeriod(LocalDateTime from, LocalDateTime to, int person_id){
+        Person applicant = personService.getLoggedInUser(person_id);
+
+     */
+
+    }
+
 
